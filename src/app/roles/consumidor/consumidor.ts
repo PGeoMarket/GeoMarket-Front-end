84dc4807a329a7205f4c70b60bdce02774ab4dc3
Peleador <daviparralopez@gmail.com>
2025-08-22 16:49:12 -0500

    prueba

---
+++ src/app/roles/consumidor/consumidor.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-consumidor',
+  imports: [],
+  templateUrl: './consumidor.html',
+  styleUrl: './consumidor.css'
+})
+export class Consumidor {
+
+}
---
+++ src/app/roles/vendedor/vendedor.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-vendedor',
+  imports: [],
+  templateUrl: './vendedor.html',
+  styleUrl: './vendedor.css'
+})
+export class Vendedor {
+
+}
