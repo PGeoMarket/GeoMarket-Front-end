bfe3532a9b56ff0868b5354b57161cb4873c53d5
PimPomUwU <daviparralopez@gmail.com>
2025-09-14 21:38:01 -0500

    A

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,14 +2,9 @@ import { Routes } from '@angular/router';
 import { AddProduct } from './features/vendedor/dialogs-seller/add-product/add-product';
 import { EditProduct } from './features/vendedor/dialogs-seller/edit-product/edit-product';
 import { ProfileSeller } from './features/vendedor/profile-seller/profile-seller';
-<<<<<<< HEAD
 import { Home } from './features/consumidor/home/home';
-import { ProfileOther } from './features/consumidor/profile-other/profile-other';
-import { FavoritePublications } from './features/consumidor/favorite-publications/favorite-publications';
-=======
 import { FavoritePublications } from './features/consumidor/favorite-publications/favorite-publications';
 import { ProfileOther } from './features/consumidor/profile-other/profile-other';
->>>>>>> origin/dev
 
 export const routes: Routes = [
     {path: 'home', component: Home},
