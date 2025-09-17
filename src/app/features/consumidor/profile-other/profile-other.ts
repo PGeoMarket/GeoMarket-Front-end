45d7a8d6742a3677749ade973cfb58c45701e297
Stiven <almeidaibarra.stiven@gmail.com>
2025-09-17 14:29:00 -0500

    Add EditPublication component to profile-other module imports

--- src/app/features/consumidor/profile-other/profile-other.ts
+++ src/app/features/consumidor/profile-other/profile-other.ts
@@ -1,9 +1,10 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { EditPublication } from "../../vendedor/edit-publication/edit-publication";
 
 @Component({
   selector: 'app-profile-other',
-  imports: [CommonModule],
+  imports: [CommonModule, EditPublication],
   templateUrl: './profile-other.html',
   styleUrl: './profile-other.css'
 })
