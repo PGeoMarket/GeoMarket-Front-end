feb772b14c898c4e6bb8ec6503e73dc3da0f6654
Stiven <almeidaibarra.stiven@gmail.com>
2025-09-17 14:45:28 -0500

    Add FAQ dialog and update seller slidebar for FAQ navigation

--- src/app/core/dialogs/dialog-registry.ts
+++ src/app/core/dialogs/dialog-registry.ts
@@ -3,6 +3,7 @@
 // Importa los componentes que se usarán como diálogos
 import { FilterByCategory } from "../../features/consumidor/dialogs/filter-by-category/filter-by-category";
 import { FilterByProximity } from "../../features/consumidor/dialogs/filter-by-proximity/filter-by-proximity";
+import { Faq } from "../../features/consumidor/faq/faq";
 import { AddProduct } from "../../features/vendedor/dialogs-seller/add-product/add-product";
 import { EditProduct } from "../../features/vendedor/dialogs-seller/edit-product/edit-product";
 
@@ -11,6 +12,8 @@ import { EditProduct } from "../../features/vendedor/dialogs-seller/edit-product
  * La clave es el nombre (string) y el valor es el componente.
  */
 export const DIALOG_COMPONENTS = {
+  //all
+  'faq':Faq,
   // Vendedor
   'add-product': AddProduct,
   'edit-product': EditProduct,
