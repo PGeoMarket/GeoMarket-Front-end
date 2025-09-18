57f198e96b9d31ed6aa4569100e22434b7d0ca58
JuanDiPosada <juandiegocifuentesposada@gmail.com>
2025-09-18 01:31:18 -0500

    Agregar componentes de diálogo para registro y FAQ en el diccionario de diálogos

--- src/app/core/dialogs/dialog-registry.ts
+++ src/app/core/dialogs/dialog-registry.ts
@@ -3,6 +3,9 @@
 // Importa los componentes que se usarán como diálogos
 import { FilterByCategory } from "../../features/consumidor/dialogs/filter-by-category/filter-by-category";
 import { FilterByProximity } from "../../features/consumidor/dialogs/filter-by-proximity/filter-by-proximity";
+import { RegisterAs } from "../../features/invitado/register-as/register-as";
+import { Register } from "../../features/invitado/register/register";
+import { Faq } from "../../features/consumidor/faq/faq";
 import { AddProduct } from "../../features/vendedor/dialogs-seller/add-product/add-product";
 import { EditProduct } from "../../features/vendedor/dialogs-seller/edit-product/edit-product";
 
