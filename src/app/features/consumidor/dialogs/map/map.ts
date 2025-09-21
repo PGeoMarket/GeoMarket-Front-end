d8431a48f4e1e6d043b287a8c4b0db4137a605c1
Karen Acero <acerokaren9.1@gmail.com>
2025-09-21 15:51:10 -0500

    mapa

--- src/app/features/consumidor/dialogs/map/map.ts
+++ src/app/features/consumidor/dialogs/map/map.ts
@@ -1,16 +1,40 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { Closedialog } from '../../../../core/dialogs/closedialog';
+import { Loader } from '@googlemaps/js-api-loader'; 
+import { DialogManager } from '../../../../core/dialogs/dialog-manager';
 
 @Component({
   selector: 'app-map',
+  standalone: true,
   imports: [Closedialog],
   templateUrl: './map.html',
   styleUrl: './map.css'
 })
-export class Map {
+export class Map implements OnInit {
+  title = 'google-maps';
 
+  ngOnInit(): void {
+    const loader = new Loader({
+      apiKey: 'AIzaSyB5z4XpgHuZimhlIFzBKd2ey_VtzcRgcvs', 
+      version: 'weekly'
+    });
 
+    loader.load().then(() => {
+      const mapElement = document.getElementById("gmap_canvas");
+      if (mapElement) {
+        new google.maps.Map(mapElement, {
+          center: { lat: 4.60971, lng: -74.08175 }, 
+          zoom: 6
+        });
+      }
+    });
+  }
+
+ private dialogManager = inject(DialogManager);
+  OnConfirmar() {
+    this.dialogManager.openDialog('select-location', {
+      data: { mode: 'create' }
+    });
+  }
 
 }
-
-
