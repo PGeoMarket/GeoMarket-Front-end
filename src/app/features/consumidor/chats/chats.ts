1737e91d11f706bc817d122022840edbb5979f70
Stiven <almeidaibarra.stiven@gmail.com>
2025-09-21 18:39:23 -0500

    Add Chats feature: implement chat list and open chat components with styling

--- src/app/features/consumidor/chats/chats.ts
+++ src/app/features/consumidor/chats/chats.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
+import { OpenChat } from "../open-chat/open-chat";
 
 @Component({
   selector: 'app-chats',
-  imports: [],
+  imports: [OpenChat],
   templateUrl: './chats.html',
   styleUrl: './chats.css'
 })
 export class Chats {
+repeat = Array.from({ length: 16 });
 
+ifOpen : boolean= false;
 }
