7a054f17ba7f35229c1f8d4f2a4597cf7854a929
Camilo <baironramos959@gmail.com>
2025-09-23 23:09:49 -0500

    admin faltaria enlazar

--- src/app/core/components/slidebars/admin-slidebar/admin-slidebar.ts
+++ src/app/core/components/slidebars/admin-slidebar/admin-slidebar.ts
@@ -1,5 +1,6 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { DialogManager } from '../../../dialogs/dialog-manager';
+import { UserDTO, UserService } from '../../../services/user-service';
 
 @Component({
   selector: 'app-admin',
@@ -7,11 +8,22 @@ import { DialogManager } from '../../../dialogs/dialog-manager';
   templateUrl: './admin-slidebar.html',
   styleUrl: './admin-slidebar.css'
 })
-export class AdminSlidebar {
+export class AdminSlidebar implements OnInit{
  private dialogManager = inject(DialogManager);
   onFaq() {
     this.dialogManager.openDialog('faq', {
       data: { mode: 'create' }
     });
+  }
+  user: UserDTO | null = null;
+  constructor(private userService:UserService){}
+  ngOnInit(): void {
+    this.getUserData();
+  }
+
+
+  getUserData() {
+    this.user = this.userService.getCurrentUser();
+    console.log(this.user);
   }
 }
