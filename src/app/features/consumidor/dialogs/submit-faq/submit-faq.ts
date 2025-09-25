7c190460d70a947be8cdc1089b100fd670fca742
Stiven <almeidaibarra.stiven@gmail.com>
2025-09-25 16:36:43 -0500

    Add SubmitFaq dialog component and integrate into FAQ flow

---
+++ src/app/features/consumidor/dialogs/submit-faq/submit-faq.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { Closedialog } from '../../../../core/dialogs/closedialog';
+
+@Component({
+  selector: 'app-submit-faq',
+  imports: [Closedialog],
+  templateUrl: './submit-faq.html',
+  styleUrl: './submit-faq.css'
+})
+export class SubmitFaq {
+
+}
