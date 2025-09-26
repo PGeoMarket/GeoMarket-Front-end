e4f13fa7b4e2db54b047a9f26c9bf92b1437508d
PimPomUwU <daviparralopez@gmail.com>
2025-09-26 01:08:19 -0500

    Revert "seller data in product-detail"
    This reverts commit fbe005af3fa7a32210e53cc71b7bc13ef75809da.

--- src/app/core/services/user-service.ts
+++ src/app/core/services/user-service.ts
@@ -3,7 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { BehaviorSubject, map, Observable, tap } from 'rxjs';
 import { CrudService } from './crud-service';
 import { ImageDTO, PublicationDTO } from './publication-service';
-import { SellerDTO } from './seller-service'; 
+import { CommentDTO } from './comment-service';
 
 
 export interface UserDTO {
@@ -25,15 +25,41 @@ export interface UserDTO {
     updated_at: string;
   };
 
-  seller?: SellerDTO;
+  seller?: {
+    id: number;
+    user_id: number;
+    nombre_tienda: string;
+    descripcion?: string | null;
+    activo: boolean;
+    puntuacion_promedio: number;
+    created_at: string;
+    updated_at: string;
+
+    coordinate: {
+      id: number;
+      created_at: string;
+      updated_at: string;
+      latitud: number;
+      longitud: number;
+      direccion: string;
+      coordinateable_type: string;
+      coordinateable_id: number;
+    };
+
+    image: ImageDTO;
+
+    phones: {
+      id: number;
+      numero_telefono: number;
+      seller_id: number;
+      created_at: string;
+      updated_at: string;
+    }[];
+  } | null;
 
   image?: ImageDTO | null;
 
-  coordinate?: CoordinateDTO | null;
-}
-
-
-export interface CoordinateDTO {
+  coordinate?: {
     id: number;
     created_at: string;
     updated_at: string;
@@ -42,6 +68,7 @@ export interface CoordinateDTO {
     direccion: string;
     coordinateable_type: string;
     coordinateable_id: number;
+  };
 }
 
 @Injectable({
@@ -133,8 +160,8 @@ export class UserService extends CrudService<UserDTO> {
   getOwnPublications() {
     const user = this.getCurrentUser();
     let user_id = user?.id;
-    console.log("id del usuario: " + user_id)
-    //console.log(`${this.API_URL}/${this.endpoint}/${user_id}?included=seller.publications.image`);
+    console.log("id del vendedor: " + user_id)
+    console.log(`${this.API_URL}/${this.endpoint}/${user_id}?included=seller.publications.image`);
 
     return this.http
       .get<any>(`${this.API_URL}/${this.endpoint}/${user_id}?included=seller.publications.image`)
