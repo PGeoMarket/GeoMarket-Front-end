33845fef9e3ee46c0fb2f28d44a616e2209bf233
Camilo <baironramos959@gmail.com>
2025-09-26 13:42:32 -0500

    solo cambios en el html y para que se bloquee si no preciona otro motivo

--- src/app/features/consumidor/dialogs/report/report.ts
+++ src/app/features/consumidor/dialogs/report/report.ts
@@ -1,14 +1,15 @@
 import { Component } from '@angular/core';
 import { Closedialog } from "../../../../core/dialogs/closedialog";
+import { FormsModule, NgModel } from '@angular/forms';
 
 @Component({
   selector: 'app-report',
-  imports: [Closedialog],
+  imports: [Closedialog,FormsModule],
   templateUrl: './report.html',
   styleUrl: './report.css'
 })
 export class Report {
 
-
+ otroMotivo: boolean = false;
 
 }
