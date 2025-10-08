217038d32d6c60038848fd52d3e3b4a52d64f23b
Stiven <almeidaibarra.stiven@gmail.com>
2025-10-08 14:46:50 -0500

    front actualizado

--- src/app/features/consumidor/chats/chats.ts
+++ src/app/features/consumidor/chats/chats.ts
@@ -1,21 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { OpenChat } from "../open-chat/open-chat";
 import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-chats',
-  imports: [OpenChat],
+  imports: [OpenChat, CommonModule],
   templateUrl: './chats.html',
   styleUrl: './chats.css'
 })
 export class Chats {
-repeat = Array.from({ length: 16 });
+  repeat = Array.from({ length: 16 });
+  ifOpen: boolean = false;
+  private windowWidth: number = 0;
 
-ifOpen : boolean= false;
- 
-constructor(private router:Router){}
+  constructor(private router: Router) {
+    this.windowWidth = window.innerWidth;
+  }
 
- goBack() {
-    this.router.navigate(['/']); // o la ruta que quieras
+  @HostListener('window:resize', ['$event'])
+  onResize(event: any) {
+    this.windowWidth = event.target.innerWidth;
+  }
+
+  isMobile(): boolean {
+    return this.windowWidth < 768; // md breakpoint de Tailwind
+  }
+
+  openChat() {
+    this.ifOpen = true;
+  }
+
+  closeChat() {
+    this.ifOpen = false;
+  }
+
+  goBack() {
+    this.router.navigate(['/']);
   }
 }
--- src/app/features/consumidor/open-chat/open-chat.ts
+++ src/app/features/consumidor/open-chat/open-chat.ts
@@ -1,11 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-open-chat',
-  imports: [],
+  standalone: true,
   templateUrl: './open-chat.html',
-  styleUrl: './open-chat.css'
+  styleUrls: ['./open-chat.css']
 })
 export class OpenChat {
+  @Output() closeChat = new EventEmitter<void>();
 
+  onClose() {
+    this.closeChat.emit();
+  }
 }
