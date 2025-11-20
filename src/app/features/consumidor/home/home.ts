b3710109c3ca10ede047b20c0cf2190f474ada95
PimPomUwU <daviparralopez@gmail.com>
2025-11-20 17:26:46 -0500

    fix guest see comments

--- src/app/features/consumidor/home/home.ts
+++ src/app/features/consumidor/home/home.ts
@@ -4,6 +4,7 @@ import { DialogManager } from '../../../core/dialogs/dialog-manager';
 import { Filters } from '../filters/filters';
 import { UserDTO, UserService } from '../../../core/services/user-service';
 import { CoordinateMapServiceDTO } from '../../../core/services/map-service';
+import { LoginService } from '../../../core/services/login-service';
 
 @Component({
   selector: 'app-home',
@@ -15,22 +16,34 @@ export class Home {
   isAbierto: boolean = false;
   dialogManager = inject(DialogManager);
   user: UserDTO | null = null;
-  user_coordinate!: CoordinateMapServiceDTO|null;
-  constructor(private userService: UserService) {}
+  user_coordinate!: CoordinateMapServiceDTO | null;
+  constructor(private userService: UserService, private loginService: LoginService) { }
 
   ngOnInit(): void {
     this.getUserData();
-    this.user_coordinate = this.userService.getTemporaryLocation()||null;    
+    this.user_coordinate = this.userService.getTemporaryLocation() || null;
+    if (!!!this.user?.activo) {
+      this.loginService.logout().subscribe({
+        next: () => {
+          console.log('Logout exitoso');
+        },
+        error: () => {
+          // Logout local aunque falle el servidor
+          localStorage.clear();
+          this.userService.clearUserData();
+        }
+      });
 
+    }
   }
 
   abrirFiltros(event: MouseEvent) {
     event.stopPropagation(); // evita que cierre de inmediato
-    this.isAbierto =  true;
+    this.isAbierto = true;
   }
 
   cerrarMenus() {
-    this.isAbierto =  false;
+    this.isAbierto = false;
   }
 
 
@@ -44,19 +57,20 @@ export class Home {
       data: { mode: 'select' },
       onClose: (res) => {
         console.log('cerrado con', res);
-        
+
         //Actualizar la ubicación del usuario si se seleccionó una nueva
         this.user_coordinate = this.userService.getTemporaryLocation()!;
-        
+
         console.log("a", this.userService.getTemporaryLocation());
-        
+
       }
     });
 
   }
 
 
-    getUserData() {
+  getUserData() {
+    this.userService.getMe().subscribe();
     this.user = this.userService.getCurrentUser();
     console.log(this.user);
   }
