2eb129ae157c06192b4cd2a14b4b85127536785b
PimPomUwU <daviparralopez@gmail.com>
2025-11-28 13:03:26 -0500

    cerrar detail al dar click a otra publication

--- src/app/features/consumidor/publications/publications.ts
+++ src/app/features/consumidor/publications/publications.ts
@@ -268,13 +268,12 @@ export class Publications implements OnInit {
   private closeTimeout: any;
 
   openProductDetail(publication: PublicationDTO) {
-    if (this.publication_selected) return;
+    if (this.open) return this.closePublicationDetail(); //ya no cierra al dar click a otra publicacion
+
     if (this.closeTimeout) {
       clearTimeout(this.closeTimeout);
       this.closeTimeout = undefined;
     }
-
-    //if (this.open) return this.closePublicationDetail(); //ya no cierra al dar click a otra publicacion
 
     this.publication_selected = publication;
     this.open = true;
